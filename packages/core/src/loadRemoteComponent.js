dfcd6036aac6bf94c8ef53149ff46bcf5336ca84
zhangw <935279748@qq.com>
2020-02-04 15:48:30 +0800

    update

--- packages/core/src/loadRemoteComponent.js
+++ packages/core/src/loadRemoteComponent.js
@@ -1,21 +1,18 @@
-import React from "react";
-import * as ReactNative from "react-native";
-import ReactNavigation from "react-navigation";
-import ReactNavigationStack from "react-navigation-stack";
+import React from 'react';
+import * as ReactNative from 'react-native';
+import ReactNavigation from 'react-navigation';
 
 /**
  * handle the webpack external module
  * @param {string} name module name
  */
 function require(name) {
-  if (name == "REACT") {
+  if (name == 'REACT') {
     return React;
-  } else if (name == "REACT_NATIVE") {
+  } else if (name == 'REACT_NATIVE') {
     return ReactNative;
-  } else if (name == "REACT_NAVIGATION") {
+  } else if (name == 'REACT_NAVIGATION') {
     return ReactNavigation;
-  } else if (name == "REACT_NAVIGATION_STACK") {
-    return ReactNavigationStack;
   } else {
     throw `require unsupported external module: ${name}`;
   }
@@ -32,7 +29,7 @@ export default function loadRemoteComponent(url, fetch) {
     .then(body => {
       // webpack commonjs 规范模块
       const exports = {};
-      const createComponent = new Function("exports", "require", body);
+      const createComponent = new Function('exports', 'require', body);
       createComponent(exports, require);
       return exports.__esModule ? exports.default : exports;
     });
