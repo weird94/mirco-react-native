1254b8b5168a14c57fedc80d6f146997c241db99
zhangw <935279748@qq.com>
2020-02-05 21:34:12 +0800

    update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,4 @@
 export { default as loadRemoteComponent } from './loadRemoteComponent';
 export { default as RemoteComponentContainer } from './Container';
 export { createContainer } from './createContainer';
-export const CONTAINER = Symbol('symbol for dynamic-rn-container');
+export const CONTAINER = '$$dynamic_rn_container';
