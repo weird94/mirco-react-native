6daac0790ab129ce741699009a44d98f70e4ac2a
zhangw <935279748@qq.com>
2020-02-09 00:21:49 +0800

    update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,5 @@ export { default as RemoteComponentContainer } from './Container';
 export { createContainer } from './createContainer';
 export const CONTAINER = '$$dynamic_rn_container';
 export { default as openDynamicRNPage } from './openDynamicRNPage';
+export { default as Loading } from './Loading';
+export { default as ErrorTips } from './ErrorTips';
