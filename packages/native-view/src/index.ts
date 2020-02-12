34d08443a8b2b83f20e868e6fef325e67b55fb83
zhangw <935279748@qq.com>
2020-02-12 22:23:28 +0800

    update

---
+++ packages/native-view/src/index.ts
@@ -0,0 +1,7 @@
+import { requireNativeComponent } from 'react-native';
+
+const TrainKeyboard = requireNativeComponent('TrainKeyboard');
+
+const WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker');
+
+export { TrainKeyboard, WheelCurvedPickerNative };
